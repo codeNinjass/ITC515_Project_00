be9f32da5279ede8e7ec22b3f04dee97b93332fc
bednidhi <bednidhi.rijal2013@gmail.com>
2020-08-26 15:38:13 +1000

    update variable name
    make variable names consistent ie. all function names start with small letter and follow camel case style

--- src/library/fixbook/fIX_bOOK_cONTROL.java
+++ src/library/fixbook/fIX_bOOK_cONTROL.java
@@ -4,35 +4,35 @@ import library.entities.Library;
 
 public class fIX_bOOK_cONTROL {
 	
-	private FixBookUI Ui;
+	private FixBookUi ui;
 	private enum CoNtRoL_StAtE { INITIALISED, READY, FIXING };
-	private CoNtRoL_StAtE StAtE;
+	private ControlState state;
 	
-	private Library LiBrArY;
-	private Book CuRrEnT_BoOk;
+	private Library library;
+	private Book currentBook;
 
 
 	public fIX_bOOK_cONTROL() {
-		this.LiBrArY = Library.GeTiNsTaNcE();
-		StAtE = CoNtRoL_StAtE.INITIALISED;
+		this.library = library.GeTiNsTaNcE();
+		state = CoNtRoL_StAtE.INITIALISED;		//StAtE-->state
 	}
 	
 	
-	public void SeT_Ui(FixBookUI ui) {
-		if (!StAtE.equals(CoNtRoL_StAtE.INITIALISED)) 
+	public void SeT_Ui(FixBookUi ui) {
+		if (!state.equals(CoNtRoL_StAtE.INITIALISED)) 
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 			
 		this.Ui = ui;
 		ui.SeT_StAtE(FixBookUI.uI_sTaTe.READY);
-		StAtE = CoNtRoL_StAtE.READY;		
+		state = CoNtRoL_StAtE.READY;		//StAtE --> state		
 	}
 
 
-	public void BoOk_ScAnNeD(int BoOkId) {
-		if (!StAtE.equals(CoNtRoL_StAtE.READY)) 
+	public void BoOk_ScAnNeD(int bookId) {		//BoOkId --> bookId
+		if (!state.equals(CoNtRoL_StAtE.READY)) 
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 			
-		CuRrEnT_BoOk = LiBrArY.gEt_BoOk(BoOkId);
+		currentBook = library.gEt_BoOk(bookId);		//LiBrArY-->library, BoOkId --> bookId, CuRrEnT_BoOk = currentBook
 		
 		if (CuRrEnT_BoOk == null) {
 			Ui.dIsPlAy("Invalid bookId");
